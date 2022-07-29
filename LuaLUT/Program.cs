a04ec6b421b4c2a687141da37172f0585c342916
NULL511 <null511@outlook.com>
2022-07-29 01:45:16 -0400

    fix test protection

--- LuaLUT/Program.cs
+++ LuaLUT/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("LutBaker.Tests")]
+[assembly: InternalsVisibleTo("LuaLUT.Tests")]
 
 namespace LutBaker
 {
